f223179296a1a3a54d85c05fbfa321f651c4d80c
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-12-31 09:40:30 -0500

    Improved styling by rounding corners and editing colors.

--- src/components/search-sidebar/search-sidebar.component.jsx
+++ src/components/search-sidebar/search-sidebar.component.jsx
@@ -4,7 +4,11 @@ import './search-sidebar.styles.scss';
 function SearchSidebar() {
   return (
     <div className="search-sidebar-container">
-      <h1>Search Sidebar</h1>
+      <input
+        className="search-input form-control"
+        type="text"
+        placeholder="Search for a restaurant"
+      />
       <div className="search-bar">
         {foodTypes.map((foodType) => (
           <button className="btn btn-primary button" key={foodType}>
